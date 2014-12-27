2c9b66be05d50798dab08db931fb0038bb5959aa
Li Shunan <lishunan246@gmail.com>
2014-12-27 19:34:51 +0800

    working

--- src/main/java/com/lishunan246/mdeditor/MainWindow.java
+++ src/main/java/com/lishunan246/mdeditor/MainWindow.java
@@ -1,11 +1,23 @@
 package com.lishunan246.mdeditor;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import org.markdown4j.Markdown4jProcessor;
 
 /**
  * Created by lishunan on 14-12-27.
  */
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame implements ActionListener, DocumentListener {
+
+    protected JLabel htmlArea;
+    protected JTextArea mdArea;
 
     MainWindow()
     {
@@ -14,6 +26,74 @@ public class MainWindow extends JFrame{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JMenuBar menuBar=new JMenuBar();
+        JMenu menu=new JMenu("File");
+        menuBar.add(menu);
+
+        JMenuItem menuItem=new JMenuItem("Open");
+        menuItem.setActionCommand("open");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        mdArea = new JTextArea();
+        mdArea.getDocument().addDocumentListener(this);
+
+        htmlArea = new JLabel();
+        htmlArea.setVerticalAlignment(SwingConstants.TOP);
+
+        JSplitPane splitPane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.setLeftComponent(new JScrollPane(mdArea));
+        splitPane.setRightComponent(new JScrollPane(htmlArea));
+        splitPane.setResizeWeight(0.5);
+        splitPane.setDividerLocation(0.5);
+        add(splitPane);
+
+        setJMenuBar(menuBar);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("open".equals(e.getActionCommand()))
+        {
+            JFileChooser chooser = new JFileChooser();
+            FileFilter filter = new FileNameExtensionFilter(
+                    "Markdown Files(*.md *.txt)", "md", "txt");
+            chooser.setFileFilter(filter);
+            int returnVal = chooser.showOpenDialog(this);
+            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                System.out.println("You chose to open this file: " +
+                        chooser.getSelectedFile().getName());
+
+                //mdArea.
+            }
+        }
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        //System.out.println("insert");
+
+        sync();
+    }
+
+    private void sync() {
+        try {
+            htmlArea.setText("<html>"+new Markdown4jProcessor().process(mdArea.getText())+"<html>");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            System.out.println("parse fail");
+        }
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        //System.out.println("remove");
+        sync();
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        //System.out.println("change");
     }
 }
