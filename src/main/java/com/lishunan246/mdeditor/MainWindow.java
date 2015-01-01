34857cbfdf13a3d17c11725d8535753779377732
Li Shunan <lishunan246@gmail.com>
2015-01-01 23:26:11 +0800

    can specify css at start up

--- src/main/java/com/lishunan246/mdeditor/MainWindow.java
+++ src/main/java/com/lishunan246/mdeditor/MainWindow.java
@@ -5,30 +5,85 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.html.HTMLEditorKit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
 
 import org.markdown4j.Markdown4jProcessor;
 import com.google.common.io.Files;
+import javax.swing.text.html.StyleSheet;
 
 /**
  * Created by lishunan on 14-12-27.
  */
 public class MainWindow extends JFrame implements ActionListener, DocumentListener {
 
-    protected JLabel htmlArea;
+    protected JEditorPane jEditorPane;
     protected JTextArea mdArea;
+    HTMLEditorKit kit;
 
     MainWindow()
     {
+        mdArea = new JTextArea();
+        mdArea.getDocument().addDocumentListener(this);
+
+        jEditorPane=new JEditorPane();
+        jEditorPane.setEditable(false);
+        kit = new HTMLEditorKit();
+
+        setCSS();
+
+        jEditorPane.setEditorKit(kit);
+
+        initMainWindow();
+    }
+
+    private void setCSS() {
+        JFileChooser chooser = new JFileChooser();
+        FileFilter filter = new FileNameExtensionFilter("CSS Files (*.css)", "css");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(this);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " +chooser.getSelectedFile().getName());
+            try {
+                URL css=chooser.getSelectedFile().toURI().toURL();
+
+                StyleSheet s = new StyleSheet();
+
+                s.importStyleSheet(css);
+
+                kit.setStyleSheet(s);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                System.out.println("cannot open css");
+            }
+        }
+    }
+
+    private void initMainWindow() {
+        JSplitPane splitPane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.setLeftComponent(new JScrollPane(mdArea));
+        splitPane.setRightComponent(new JScrollPane(jEditorPane));
+        splitPane.setResizeWeight(0.5);
+        splitPane.setDividerLocation(0.5);
+        add(splitPane);
+
         setTitle("MarkDown Editor");
         setSize(800,600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        initMenuBar();
+        setVisible(true);
+    }
+
+    private void initMenuBar() {
         JMenuBar menuBar=new JMenuBar();
         JMenu menu=new JMenu("File");
         menuBar.add(menu);
@@ -43,79 +98,25 @@ public class MainWindow extends JFrame implements ActionListener, DocumentListen
         menuItem.addActionListener(this);
         menu.add(menuItem);
 
-        mdArea = new JTextArea();
-        mdArea.getDocument().addDocumentListener(this);
+        menu =new JMenu("CSS");
+        menuBar.add(menu);
 
-        htmlArea = new JLabel();
-        htmlArea.setVerticalAlignment(SwingConstants.TOP);
+        menuItem=new JMenuItem("Set");
+        menuItem.setActionCommand("set css");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
 
-        JSplitPane splitPane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        splitPane.setLeftComponent(new JScrollPane(mdArea));
-        splitPane.setRightComponent(new JScrollPane(htmlArea));
-        splitPane.setResizeWeight(0.5);
-        splitPane.setDividerLocation(0.5);
-        add(splitPane);
+        menuItem=new JMenuItem("Reset");
+        menuItem.setActionCommand("reset css");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
 
         setJMenuBar(menuBar);
-        setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if ("open".equals(e.getActionCommand()))
-        {
-            JFileChooser chooser = new JFileChooser();
-            FileFilter filter = new FileNameExtensionFilter("Markdown Files (*.md)", "md");
-            chooser.setFileFilter(filter);
-            int returnVal = chooser.showOpenDialog(this);
-            if(returnVal == JFileChooser.APPROVE_OPTION) {
-                System.out.println("You chose to open this file: " +chooser.getSelectedFile().getName());
-
-                try {
-                    String string = Files.toString(chooser.getSelectedFile(), Charset.defaultCharset());
-                    mdArea.setText(string);
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                    System.out.println("Fail to open file");
-                }
-            }
-        }
-        else if ("save as".equals(e.getActionCommand()))
-        {
-            JFileChooser chooser=new JFileChooser();
-            FileFilter filter = new FileNameExtensionFilter("Markdown Files (*.md)", "md");
-            chooser.setFileFilter(filter);
-            if(chooser.showSaveDialog(this)==JFileChooser.APPROVE_OPTION)
-            {
-                String path=chooser.getSelectedFile().getAbsolutePath();
-                System.out.println(path);
-
-                if(!path.endsWith(".md"))
-                {
-                    path+=".md";
-                }
-                try {
-                    FileWriter writer=new FileWriter(path);
-                    writer.write(mdArea.getText());
-                    writer.close();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                    System.out.println("fail to write file");
-                }
-            }
-        }
-    }
-
-    @Override
-    public void insertUpdate(DocumentEvent e) {
-        //System.out.println("insert");
-
-        sync();
     }
 
     private void sync() {
         try {
-            htmlArea.setText("<html>"+new Markdown4jProcessor().process(mdArea.getText())+"<html>");
+            jEditorPane.setText("<html>"+new Markdown4jProcessor().process(mdArea.getText())+"<html>");
         } catch (IOException e1) {
             e1.printStackTrace();
             System.out.println("parse fail");
@@ -123,13 +124,93 @@ public class MainWindow extends JFrame implements ActionListener, DocumentListen
     }
 
     @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("open".equals(e.getActionCommand()))
+        {
+            openMD();
+        }
+        else if ("save as".equals(e.getActionCommand()))
+        {
+            saveAsMD();
+        }
+        else if("reset css".equals(e.getActionCommand()))
+        {
+            System.out.println("reset css");
+
+            StyleSheet s = new StyleSheet();
+
+            try {
+                s.importStyleSheet(new URL("file:///home/lishunan/markdown.css"));
+            } catch (MalformedURLException e1) {
+                e1.printStackTrace();
+                System.out.println("css invalid");
+            }
+
+            //kit.getStyleSheet().addStyleSheet(s);
+            jEditorPane=new JEditorPane();
+            jEditorPane.setEditable(false);
+
+            kit = new HTMLEditorKit();
+            kit.setStyleSheet(s);
+            jEditorPane.setEditorKit(kit);
+            revalidate();
+            sync();
+        }
+    }
+
+    private void openMD() {
+        JFileChooser chooser = new JFileChooser();
+        FileFilter filter = new FileNameExtensionFilter("Markdown Files (*.md)", "md");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(this);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " +chooser.getSelectedFile().getName());
+
+            try {
+                String string = Files.toString(chooser.getSelectedFile(), Charset.defaultCharset());
+                mdArea.setText(string);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                System.out.println("Fail to open file");
+            }
+        }
+    }
+
+    private void saveAsMD() {
+        JFileChooser chooser=new JFileChooser();
+        FileFilter filter = new FileNameExtensionFilter("Markdown Files (*.md)", "md");
+        chooser.setFileFilter(filter);
+        if(chooser.showSaveDialog(this)==JFileChooser.APPROVE_OPTION)
+        {
+            String path=chooser.getSelectedFile().getAbsolutePath();
+            System.out.println(path);
+
+            if(!path.endsWith(".md"))
+            {
+                path+=".md";
+            }
+            try {
+                FileWriter writer=new FileWriter(path);
+                writer.write(mdArea.getText());
+                writer.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                System.out.println("fail to write file");
+            }
+        }
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        sync();
+    }
+
+    @Override
     public void removeUpdate(DocumentEvent e) {
-        //System.out.println("remove");
         sync();
     }
 
     @Override
     public void changedUpdate(DocumentEvent e) {
-        //System.out.println("change");
     }
 }
